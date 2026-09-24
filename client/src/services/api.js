877d816c29b8d85858df2f3c61bae4b3e608d204
Vijay <vijaydange029@gmail.com>
2026-09-24 10:42:44 +0530

    feat: connect frontend to production API

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+   baseURL: "https://rating-platform-05rv.onrender.com/api",
    headers: {
       "Content-Type": "application/json",
    },
